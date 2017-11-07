0a32a9a9d4420d0242a17c4996a58a565ffdd836
thort037 <thort037@abc.com>
2017-11-07 13:33:36 -0800

    Fixed my merging files code

--- sec7_lec72_merging_textfiles.py
+++ sec7_lec72_merging_textfiles.py
@@ -1,5 +1,5 @@
 import datetime
-#import glob2
+import glob
 
 
 
@@ -7,14 +7,14 @@ import datetime
 
 filename=datetime.datetime.now()
 lst = ["files/sec7lec72/file1.txt", "files/sec7lec72/file2.txt", "files/sec7lec72/file3.txt"]
-
+'''
 def read_files():
     for i in lst:
         f = open(i, "r")
         i = f.read()
         f.close()
         print (i)
-
+'''
 def create_file_fill():
     with open("files/" + filename.strftime("%Y-%m-%d-%H-%M-%s") + ".txt", "w") as file:
         file.write("")
@@ -28,14 +28,13 @@ def create_file_fill():
 
 
 create_file_fill()
-read_files()
 
 
-'''
-filenames = glob2.glob("files/sec7lec72/*.txt")
+filenames = glob.glob("files/sec7lec72/*.txt")
 
-with open(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%s") + ".txt") as file:
+with open("files/" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%s") + ".txt", "w") as file:
     for filename in filenames:
         with open (filename, "r") as f:
             file.write(f.read() + "\n")
-'''
+
+
