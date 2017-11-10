0b0f0f7c4336307618064c000e3fac4d7ce2b063
thort037 <thort037@abc.com>
2017-11-10 11:48:34 -0800

    Latest work

--- app1_online_dictionary/app1_dictionary.py
+++ app1_online_dictionary/app1_dictionary.py
@@ -9,8 +9,14 @@ def define(w):
     # w = w.lower() another way to ensure word case is the same
     if w in words:
         return words[w]
-    elif len(get_close_matches(w, words.keys(), cutoff=.9)) > 0:
-        return "Did you possibley mean \"%s\" instead? " % get_close_matches(w, words.keys())[0]
+    elif len(get_close_matches(w, words.keys())) > 0:
+        yn = input("Did you possibley mean \"%s\" instead?  Enter 'Y' or 'N' " % get_close_matches(w, words.keys())[0])
+        if yn == "Y":
+            return words[get_close_matches(w, words.keys())[0]]
+        elif yn == "N":
+            return "The word does not exist try again"
+        else:
+            return "We didn't understand your entry"
     else:
         return "The word does not exist try again"
 
